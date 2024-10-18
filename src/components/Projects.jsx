fbc18bcd5119e79b1dcce4e6ee839770bc4989cb
Dorian Taponzing <thebreezyguy@Dorians-MacBook-Pro.local>
2024-10-18 13:05:25 -0400

    Projects Section Added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,75 @@
 import React from "react";
+import projImg from "../assets/template-project-screenshot.png";
 
 const Projects = () => {
   return (
     <div id="projects-container">
       <h1>Projects</h1>
+      <div className="project-block">
+        <img className="project-image" src={projImg} alt="matap-img" />
+        <h2 className="project-title">TODO List App</h2>
+        <ul className="language-list">
+          <li>HTML</li>
+          <li>CSS</li>
+        </ul>
+        <ul className="project-links">
+          <li>
+            <a href="">View Project</a>
+          </li>
+          <li>
+            <a href="">View Code</a>
+          </li>
+        </ul>
+      </div>
+      <div className="project-block">
+        <img className="project-image" src={projImg} alt="matap-img" />
+        <h2 className="project-title">MATAP - Food Ordering System App</h2>
+        <ul className="language-list">
+          <li>HTML</li>
+          <li>CSS</li>
+        </ul>
+        <ul className="project-links">
+          <li>
+            <a href="">View Project</a>
+          </li>
+          <li>
+            <a href="">View Code</a>
+          </li>
+        </ul>
+      </div>
+      <div className="project-block">
+        <img className="project-image" src={projImg} alt="matap-img" />
+        <h2 className="project-title">Memory Game</h2>
+        <ul className="language-list">
+          <li>HTML</li>
+          <li>CSS</li>
+        </ul>
+        <ul className="project-links">
+          <li>
+            <a href="">View Project</a>
+          </li>
+          <li>
+            <a href="">View Code</a>
+          </li>
+        </ul>
+      </div>
+      <div className="project-block">
+        <img className="project-image" src={projImg} alt="matap-img" />
+        <h2 className="project-title">Schedule Management Web App</h2>
+        <ul className="language-list">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+        </ul>
+        <ul className="project-links">
+          <li>
+            <a href="">View Project</a>
+          </li>
+          <li>
+            <a href="">View Code</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
